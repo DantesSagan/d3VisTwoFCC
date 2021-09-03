a155c485f0759abec739f16b7b2d9cac79089c47
lexsander ikhaylov <mihaleksval@gmail.com>
2021-09-03 21:12:01 +0500

    circles doesnt showing :D

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import * as d3 from 'd3';
-import { extent, min } from 'd3';
+import { extent } from 'd3';
 
 export default function App() {
   const [url] = useState(
@@ -12,9 +12,12 @@ export default function App() {
   const height = 800;
   const padding = 120;
   useEffect(() => {
+    // Getting XMLTttpReques from url
     req.open('GET', url, true);
     req.onload = () => {
+      // Parsing json fomr url
       data = JSON.parse(req.responseText);
+      // Our data which contains data's what we need
       values = data;
       drawCanvas();
       generateScales();
@@ -23,21 +26,22 @@ export default function App() {
       infoText();
     };
     req.send();
+    // Declare global data for using json data's
     let data;
     let values = [];
-
+    // Declare global xScale and yScale parameters
     let xScale;
     let yScale;
-
-    let xValue = values.map((item) => item.Year);
-    let yValue = (item) => item.Time;
-
+    // Declare global xAxisScale and yAxisScale parameters
     let xAxisScale;
     let yAxisScale;
-
+    // Declare global svg parameter
     let svg = d3.select('svg');
+    // Declare global color parameter
+    let color = d3.scaleOrdinal(d3.schemeCategory10);
 
     const infoText = () => {
+      // Text notaion for declaring Title, Legend and Time paremeter's
       let textContainer = d3
         .select('svg')
         .append('svg')
@@ -78,33 +82,37 @@ export default function App() {
     //   );
     // };
 
+    // Declare global drawCanvas which using like background canvas contains all our visualization data
     const drawCanvas = () => {
-      svg.attr('width', width);
-      svg.attr('height', height);
+      svg.attr('width', width).attr('height', height);
     };
+
     const generateScales = () => {
-      const dataTime = values.map((item) => {
-        return item.Time;
+      // Declare parsing data Time for declaring minutes and seconds for better understanding
+      values.forEach((item) => {
+        const parsedTime = item.Time.split(':');
+        item.Time = new Date(1970, 0, 1, 0, parsedTime[0], parsedTime[1]);
       });
+      // Declare yScale our data parameters
       yScale = d3
         .scaleLinear()
-        .domain(extent(dataTime))
-        .range([0, height - 2 * padding]);
-
+        .domain(extent(values))
+        .range([padding, height - padding]);
+      // Declare formatting dataYear for parsing data time
       const dataYear = values.map((item) => {
         return new Date(item.Year);
       });
-
+      // Declare xScale our data parameters
       xScale = d3
         .scaleLinear()
         .domain(extent(dataYear))
         .range([padding, width - padding]);
-
+      // Declare xAxisScale what scaling dataYear
       xAxisScale = d3
         .scaleLinear()
         .domain(extent(dataYear))
         .range([padding, width - padding]);
-
+      // Declare yAxisScale what scaling dataTime
       yAxisScale = d3
         .scaleLinear()
         .domain(
@@ -112,10 +120,11 @@ export default function App() {
             return item.Time;
           })
         )
-        .range([height - padding, padding]);
+        .range([padding, height - padding]);
     };
 
     const drawCircles = () => {
+      // Declare tooltip what decribe information which contains every info circle
       const tooltip = d3
         .select('.hoverHolder')
         .append('div')
@@ -123,9 +132,9 @@ export default function App() {
         .style('visibility', 'hidden')
         .style('width', 'auto')
         .style('height', 'auto');
-
+      // Declaring circles by x and y axis
       svg
-        .selectAll('circle')
+        .selectAll('.dot')
         .data(values)
         .enter()
         .append('circle')
@@ -134,29 +143,39 @@ export default function App() {
           return item.Year;
         })
         .attr('data-yvalue', (item) => {
-          return item.Time;
+          return item.Time.toISOString();
+        })
+        .attr('cx', (item) => {
+          return xScale(item.Year);
+        })
+        .attr('cy', (item) => {
+          return yScale(item.Time);
+        })
+        .style('fill', (item) => {
+          return color(item.Doping !== '');
         })
         .attr('r', 5);
 
-      svg
-        .selectAll('text')
-        .data(values)
-        .enter()
-        .append('text')
-        .text(
-          () =>
-            xScale +
-            // .toFixed(1).replace(/(\d)\,(\d{3})/g, /(d)(d{3})/g)
-            ',' +
-            yScale
-        )
-        .attr('x', (item) => xScale(item.Year))
-        .attr('y', (item) => yScale(item.Time));
+      // svg
+      //   .selectAll('text')
+      //   .data(values)
+      //   .enter()
+      //   .append('text')
+      //   .text(
+      //     () =>
+      //       xScale +
+      //       // .toFixed(1).replace(/(\d)\,(\d{3})/g, /(d)(d{3})/g)
+      //       ',' +
+      //       yScale
+      //   )
+      //   .attr('x', (item) => xScale(item.Year))
+      //   .attr('y', (item) => yScale(item.Time));
     };
-
+    // Declare generateAxis what create y and x axis in format y(M:S) and x(tick)
     const generateAxis = () => {
-      const xAxis = d3.axisBottom(xAxisScale);
-      const yAxis = d3.axisLeft(yAxisScale);
+      const formatTime = d3.timeFormat('%M:%S');
+      const xAxis = d3.axisBottom(xAxisScale).tickFormat(d3.format('d'));
+      const yAxis = d3.axisLeft(yAxisScale).tickFormat(formatTime);
       svg
         .append('g')
         .call(xAxis)
